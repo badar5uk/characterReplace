5ab7725919a667a7318de72ad464f108feeb5ce5
bader <bader5uk@hotmail.com>
2024-09-22 13:04:44 +0400

    character remove using replace method

---
+++ removeCharacter.java
@@ -0,0 +1,11 @@
+public class removeCharacter {
+    public static void removeCharacter(){
+        String myWord = "Welcome to my code, I hope you like it";
+        String characters = myWord.replace('o',' ');
+        System.out.println(characters);
+    }
+
+    public static void main(String[]args){
+       removeCharacter();
+    }
+}
